737e1c90d676904667fcc1db36f3310c21f90131
Emil <nailami60@mail.ru>
2019-10-15 02:27:30 +0300

    added 5 task

--- task1.cpp
+++ task1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <omp.h>
-#include "task4.h"
+#include <limits>
 
 #define NUM_THREADS 8
 
@@ -14,15 +14,107 @@ void task3();
 
 void task4();
 
+void task5();
+
+
+
 int main() {
     //task1();
     //task2();
     //task3();
-    task4();
+    //task4();
+    task5();
 }
 
-void task4() {
+void task5() {
+    int COUNT_OF_THREADS = 4;
+    omp_set_dynamic(0);
+    omp_set_num_threads(COUNT_OF_THREADS);
+    int d[6][8];
+    for (int i = 0; i < sizeof(d); i++) {
+        for (int j = 0; j < sizeof(d[i]); j++) {
+            d[i][j] = rand() % 100;
+        }
+    }
+#pragma omp parallel sections
+    {
+#pragma omp section
+        {
+            int average = 0;
+            for (int i = 0; i < sizeof(d); i++) {
+                for (int j = 0; j < sizeof(d[i]); j++) {
+                    average += d[i][j];
+                }
+            }
+            average /= sizeof(d) * sizeof(d[0]);
+            printf("Hello, I'm thread number %d. Average = %d\n", omp_get_thread_num(), average);
 
+
+        }
+#pragma omp section
+        {
+            int max = std::numeric_limits<int>::min();
+            int min = std::numeric_limits<int>::max();
+            for (int i = 0; i < sizeof(d); i++) {
+                for (int j = 0; j < sizeof(d[i]); j++) {
+                    if (d[i][j] > max) {
+                        max = d[i][j];
+                    }
+                    if (d[i][j] < min) {
+                        min = d[i][j];
+                    }
+                }
+            }
+            printf("Hello, I'm thread number %d; Min = %d; Max = %d\n", omp_get_thread_num(), min, max);
+
+        }
+#pragma omp section
+        {
+            int multiple_three_count = 0;
+            for (int i = 0; i < sizeof(d); i++) {
+                for (int j = 0; j < sizeof(d[i]); j++) {
+                    if (d[i][j] % 3 == 0) {
+                        multiple_three_count++;
+                    }
+                }
+            }
+            printf("Hello, I'm thread number %d;\n; Multiple three count = %d\n", omp_get_thread_num(),
+                   multiple_three_count);
+
+        }
+    }
+}
+
+
+void task4() {
+    int num = 0;
+    printf("Insert count of numbers: ");
+    scanf("%d", &num);
+    int a[num], b[num];
+    for (int i = 0; i < num; i++) {
+        a[i] = rand() % (10 - 1) + 1;
+        b[i] = rand() % (10 - 1) + 1;
+    }
+    int min_a = std::numeric_limits<int>::max();
+    int max_b = std::numeric_limits<int>::min();
+#pragma omp parallel
+    {
+        if (omp_get_thread_num() == 0) {
+            for (int i = 0; i < num; i++) {
+                if (a[i] < min_a) {
+                    min_a = a[i];
+                }
+            }
+        } else {
+            for (int i = 0; i < num; i++) {
+                if (b[i] > max_b) {
+                    max_b = b[i];
+                }
+            }
+        }
+    }
+    printf("Min a: %d\n", min_a);
+    printf("Max b: %d\n", max_b);
 }
 
 void task3() {
@@ -31,6 +123,7 @@ void task3() {
     scanf("%d", &a);
     printf("\nPrint b: ");
     scanf("%d", &b);
+    omp_set_dynamic(0);
 
     omp_set_num_threads(4);
 #pragma omp parallel private(a) firstprivate(b)
@@ -47,9 +140,9 @@ void task3() {
         int thread_num = omp_get_thread_num();
         a -= omp_get_thread_num();
         b -= omp_get_thread_num();
-        printf("Inside second block %d Thread: a = %d, b = %d\n",thread_num,a,b);
+        printf("Inside second block %d Thread: a = %d, b = %d\n", thread_num, a, b);
     }
-    printf("After second block: a= %d, b = %d", a,b);
+    printf("After second block: a= %d, b = %d", a, b);
 }
 
 
--- task4.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by emil on 15.10.2019.
-//
-
-#include "task4.h"
